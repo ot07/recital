5da2d945d21a93f81e8a1172a245e5097bb076c0
ot07 <ot07neoki@gmail.com>
2022-05-24 20:38:10 +0900

    🚧 Create pianoroll prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,149 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MusicPlayer, Track, Instrument } from "./components/audio/MusicPlayer";
+import { Midi } from "@tonejs/midi";
+import { useEffect, useState } from "react";
+import * as Tone from "tone";
+
+import "./App.css";
+import { PianoRoll } from "./components/ui/PianoRoll/PIanoRoll";
+
+const MIDI_FILE = "230_bpm_multitrack.mid";
 
 function App() {
+  const [playbackState, setPlaybackState] = useState<any>("stopped");
+  const [midi, setMidi] = useState<any>();
+  const [tempos, setTempos] = useState<any>([]);
+  const [timeSignatures, setTimeSignatures] = useState<any>([]);
+
+  useEffect(() => {
+    const readMidiFile = async () => {
+      const midi = await Midi.fromUrl(MIDI_FILE);
+      setMidi(midi);
+    };
+    readMidiFile();
+  }, []);
+
+  useEffect(() => {
+    if (midi) {
+      setTempos(
+        midi.header.tempos.map((tempo: any) => {
+          return {
+            bpm: Math.round(tempo.bpm),
+            time: `${Math.round(
+              (tempo.ticks * Tone.Transport.PPQ) / midi.header.ppq
+            )}i`,
+          };
+        })
+      );
+
+      setTimeSignatures(
+        midi.header.timeSignatures.map((timeSignature: any) => {
+          return {
+            timeSignature: timeSignature.timeSignature,
+            time: `${Math.round(
+              (timeSignature.ticks * Tone.Transport.PPQ) / midi.header.ppq
+            )}i`,
+          };
+        })
+      );
+    }
+  }, [midi]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <p>
+        <button
+          onClick={() =>
+            setPlaybackState((state: any) =>
+              state === "started" ? "paused" : "started"
+            )
+          }
         >
-          Learn React
-        </a>
-      </header>
+          {playbackState === "started" ? "pause" : "start"}
+        </button>
+        <button onClick={() => setPlaybackState("stopped")}>stop</button>
+        <button
+          onClick={() =>
+            setTempos((prevTempos: any) =>
+              prevTempos.map((tempo: any) => {
+                return { ...tempo, bpm: 150 };
+              })
+            )
+          }
+        >
+          change tempo to 150
+        </button>
+        <button
+          onClick={() =>
+            setTimeSignatures((prevTimeSignatures: any) =>
+              prevTimeSignatures.map((timeSignature: any) => {
+                return { ...timeSignature, timeSignature: [5, 4] };
+              })
+            )
+          }
+        >
+          change time signature to 5/4
+        </button>
+      </p>
+      <PianoRoll notes={midi ? midi.tracks[2].notes : []} />
+
+      <MusicPlayer
+        tempos={tempos}
+        timeSignatures={timeSignatures}
+        playbackState={playbackState}
+        volume={-18}
+      >
+        {midi
+          ? midi.tracks.map((track: any, i: any) => (
+              <Track
+                key={i}
+                // Array of several types
+                notes={track.notes.map((note: any) => {
+                  return {
+                    name: note.name,
+                    duration: `${Math.round(
+                      (note.durationTicks * Tone.Transport.PPQ) /
+                        midi.header.ppq
+                    )}i`,
+                    time: `${Math.round(
+                      (note.ticks * Tone.Transport.PPQ) / midi.header.ppq
+                    )}i`,
+                    velocity: note.velocity,
+                  };
+                })}
+                volume={0}
+                pan={0}
+                // Callback for every tick
+                onStepPlay={(time: any, note: any) => {
+                  console.log(
+                    "time:",
+                    time,
+                    ", tempo:",
+                    Tone.Transport.bpm.value,
+                    ", timeSignature:",
+                    Tone.Transport.timeSignature,
+                    ", note:",
+                    note
+                  );
+                }}
+              >
+                <Instrument
+                  type="synth"
+                  oscillator={{ type: "square" }}
+                  envelope={{
+                    attack: 0.01,
+                    decay: 0.1,
+                    sustain: 0.5,
+                    release: 0.01,
+                  }}
+                  polyphony={32}
+                />
+                {/* Add effects chain here */}
+                {/* <Effect type="feedbackDelay" /> */}
+                {/* <Effect type="distortion" /> */}
+              </Track>
+            ))
+          : null}
+      </MusicPlayer>
     </div>
   );
 }
---
+++ src/components/ui/PianoRoll/PIanoRoll.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import * as PIXI from "pixi.js";
+import { Stage, Graphics } from "@inlet/react-pixi";
+import colors from "tailwindcss/colors";
+
+export const PianoRoll: React.FC<any> = ({ notes }) => {
+  const [width, setWidth] = useState(0);
+  const height = 128 * 16;
+  const resolution = Math.min(window.devicePixelRatio, 2);
+  const stageProps = {
+    options: {
+      autoDensity: true,
+      resolution: resolution || 1,
+      antialias: resolution <= 1,
+    },
+  };
+
+  useEffect(() => {
+    const endTicks = notes.reduce(
+      (endTicks: number, note: any) =>
+        Math.max(endTicks, note.ticks + note.durationTicks),
+      0
+    );
+    setWidth((endTicks * 32) / 96);
+  }, [notes]);
+
+  const drawNotes = React.useCallback(
+    (g: any) => {
+      g.clear();
+      g.beginFill(PIXI.utils.string2hex(colors.cyan[500]), 1);
+      for (const note of notes) {
+        g.drawRect(
+          (note.ticks * 32) / 96,
+          16 * (127 - note.midi),
+          Math.max((note.durationTicks * 32) / 96 - 2, 1),
+          16
+        );
+      }
+      g.endFill();
+    },
+    [notes]
+  );
+
+  const drawBackground = React.useCallback(
+    (g: any) => {
+      g.clear();
+      g.beginFill(0x272934);
+      g.drawRect(0, 0, width, 16 * 128);
+      g.endFill();
+
+      g.beginFill(0x1e2028);
+      for (let i = 0; i < 128; i++) {
+        if ([1, 3, 6, 8, 10].includes(i % 12)) {
+          g.drawRect(0, 16 * (127 - i), width, 16);
+        }
+      }
+      g.endFill();
+
+      g.lineStyle(0.5, 0x5a5078);
+      for (let i = 0; i < 128; i++) {
+        if ([4, 11].includes(i % 12)) {
+          g.moveTo(0, 16 * (127 - i) + 0.5);
+          g.lineTo(width, 16 * (127 - i) + 0.5);
+        }
+      }
+    },
+    [width]
+  );
+
+  return (
+    <div>
+      <Stage width={width} height={height} options={{ ...stageProps.options }}>
+        <Graphics draw={drawBackground} />
+        <Graphics draw={drawNotes} />
+      </Stage>
+    </div>
+  );
+};
